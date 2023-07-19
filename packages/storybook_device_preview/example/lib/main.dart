1407cb3fe45857208feb3b0b019115310aa3f90d
Emmanuel Lefebvre <Emmanuel_LEFEBVRE@connect-tech.sncf>
2023-07-19 13:38:55 +0200

    upgrade Flutter 3.10

--- packages/storybook_device_preview/example/lib/main.dart
+++ packages/storybook_device_preview/example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:storybook_device_preview/storybook_device_preview.dart';
 
 void main() => runApp(const MyApp());
@@ -23,8 +22,7 @@ class MyApp extends StatelessWidget {
             section: 'Button',
             name: 'Flat button',
             builder: (_, k) => MaterialButton(
-              onPressed:
-                  k.boolean(label: 'Enabled', initial: true) ? () {} : null,
+              onPressed: k.boolean(label: 'Enabled', initial: true) ? () {} : null,
               child: Text(k.text(label: 'Text', initial: 'Flat button')),
             ),
           ),
@@ -32,28 +30,29 @@ class MyApp extends StatelessWidget {
             section: 'Button',
             name: 'Raised button',
             // ignore: deprecated_member_use
-            builder: (_, k) => RaisedButton(
-              onPressed:
-                  k.boolean(label: 'Enabled', initial: true) ? () {} : null,
-              color: k.options(
-                label: 'Color',
-                initial: Colors.deepOrange,
-                options: const [
-                  Option('Red', Colors.deepOrange),
-                  Option('Green', Colors.teal),
-                ],
+            builder: (_, k) => ElevatedButton(
+              onPressed: k.boolean(label: 'Enabled', initial: true) ? () {} : null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: k.options(
+                  label: 'Color',
+                  initial: Colors.deepOrange,
+                  options: const [
+                    Option('Red', Colors.deepOrange),
+                    Option('Green', Colors.teal),
+                  ],
+                ),
+                enabledMouseCursor: k.options(
+                  label: 'Mouse Cursor',
+                  initial: null,
+                  options: const [
+                    Option('Basic', SystemMouseCursors.basic),
+                    Option('Click', SystemMouseCursors.click),
+                    Option('Forbidden', SystemMouseCursors.forbidden),
+                  ],
+                ),
+                elevation: k.slider(label: 'Elevation', initial: 0, max: 20),
+                foregroundColor: Colors.white,
               ),
-              mouseCursor: k.options(
-                label: 'Mouse Cursor',
-                initial: null,
-                options: const [
-                  Option('Basic', SystemMouseCursors.basic),
-                  Option('Click', SystemMouseCursors.click),
-                  Option('Forbidden', SystemMouseCursors.forbidden),
-                ],
-              ),
-              elevation: k.slider(label: 'Elevation', initial: 0, max: 20),
-              textColor: Colors.white,
               child: Text(k.text(label: 'Text', initial: 'Raised button')),
             ),
           ),
